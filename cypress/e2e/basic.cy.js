d85a7314ada3cf15b6b69ea47902dee3f607ab9f
arenaanimation1 <132885054+arenaanimation1@users.noreply.github.com>
2023-05-10 11:18:35 +0530

    Update basic.cy.js

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -1,8 +1,3 @@
-describe('sample render test', () => {
-  beforeEach(() => {
-    cy.visit('/');
-  });
-
   it('displays the title text', () => {
     cy.get('h1').contains('Next.js Blog Theme');
   });
